851e920643f7e5b6d8b77bdbed964d47f290058d
Irina Novakovich <irinaredhood@gmail.com>
2021-10-13 13:01:03 +0300

    JS object notation

---
+++ myMusic.js
@@ -0,0 +1,23 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Billy",
+    "title": "Piano",
+    "release_year": 1960,
+    "formats": [
+      "CD",
+      "8T"
+    ],
+    "gold": true
+  }
+];
