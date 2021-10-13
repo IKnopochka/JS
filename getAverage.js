b2d88a1e6b4824bf0d649de9020e6d0ca037e284
Irina Novakovich <irinaredhood@gmail.com>
2021-10-14 00:06:36 +0300

    CodeWars

--- getAverage.js
+++ getAverage.js
@@ -8,3 +8,15 @@ function getAverage(marks){
   
 }
 
+function defineSuit(card) {
+  // good luck
+  if(card[1] === "♣") {
+    return 'clubs';
+  } else if(card[1] === "♦") {
+    return 'diamonds';
+  } else if(card[1] === "♥") {
+    return 'hearts';
+  } else if(card[1] === "♠") {
+    return 'spades';
+  }
+}
