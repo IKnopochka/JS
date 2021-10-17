88452db2f4318af5be3ae4cd68100600333e1f8f
Irina Novakovich <irinaredhood@gmail.com>
2021-10-17 22:49:58 +0300

    Usage of timeOut function

---
+++ timeout.js
@@ -0,0 +1,14 @@
+		var names = ["Mishka", "Kathryn", "Irina", "Alina"];
+		$("h1").text("My friends");
+
+		for (var i = 0; i < names.length; i++) {
+			$("body").append("<p>" + names[i] + "</p>");
+		}
+
+		$("p").append(" is the best!!!");
+
+		$("h1").fadeTo(2000, 0.2);
+		var timeUp = function () {
+			alert("Timeout!");
+		}
+		setTimeout(timeUp, 3000);
