cb0863030332fe492407eab4cc89c69f8cf1cf93
Irina Novakovich <irinaredhood@gmail.com>
2021-10-17 22:57:48 +0300

    circle function

---
+++ circle.js
@@ -0,0 +1,9 @@
+var circle = function (x, y, radius, fillCircle) {
+        	ctx.beginPath();
+        	ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+        	if (fillCircle) {
+          		ctx.fill();
+        	} else {
+          		ctx.stroke();
+        	}
+      	};
